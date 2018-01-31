7e44042d7a9c33e319f44d01aa3c7a46ac7887de
vbareisha <bareisha89@gmail.com>
2018-01-31 22:16:47 +0300

    - Added preferences.

--- app/src/main/java/com/bareisha/smsbankinganalyst/MainActivity.java
+++ app/src/main/java/com/bareisha/smsbankinganalyst/MainActivity.java
@@ -1,11 +1,21 @@
 package com.bareisha.smsbankinganalyst;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static android.content.SharedPreferences.*;
+
+public class MainActivity extends AppCompatActivity implements OnSharedPreferenceChangeListener {
+
+    private TextView account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,45 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(smsListIntent);
             }
         });
+        account = findViewById(R.id.account);
+        setupSharedPreferences();
    }
+
+    private void setupSharedPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        account.setText(sharedPreferences.getString(getString(R.string.card_number_key), getString(R.string.card_number_default)));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.preference_settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals(getString(R.string.card_number_key))) {
+            account.setText(sharedPreferences.getString(getString(R.string.card_number_key), getString(R.string.card_number_default)));
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unregister VisualizerActivity as an OnPreferenceChangedListener to avoid any memory leaks.
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
 }
---
+++ app/src/main/java/com/bareisha/smsbankinganalyst/fragment/SettingsFragment.java
@@ -0,0 +1,87 @@
+package com.bareisha.smsbankinganalyst.fragment;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.bareisha.smsbankinganalyst.R;
+
+import static android.content.SharedPreferences.*;
+import static android.support.v7.preference.Preference.*;
+
+/**
+ * Created by Vova on 31.01.2018.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat implements OnSharedPreferenceChangeListener, OnPreferenceChangeListener {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preference);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int count = preferenceScreen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference p = preferenceScreen.getPreference(i);
+            // You don't need to set up preference summaries for checkbox preferences because
+            // they are already set up in xml using summaryOff and summary On
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+        Preference preference = findPreference(getString(R.string.card_number_key));
+        preference.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        Preference preference = findPreference(s);
+        if (null != preference) {
+            // Updates the summary for the preference
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return true;
+    }
+
+    private void setPreferenceSummary(Preference preference, String value) {
+        if (preference instanceof ListPreference) {
+            // For list preferences, figure out the label of the selected value
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value);
+            if (prefIndex >= 0) {
+                // Set the summary to that label
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else if (preference instanceof EditTextPreference) {
+            // For EditTextPreferences, set the summary to the value's simple string representation.
+            preference.setSummary(value);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+}
